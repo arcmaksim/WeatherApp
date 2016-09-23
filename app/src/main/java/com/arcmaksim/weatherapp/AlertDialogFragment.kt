eda71f1caaf51a99a83d5b34aa8d6ed2fd75dc69
Maxim Artsybasov <arcmaksim@gmail.com>
2016-09-23 22:59:39 +0300

    Updated AlertDialogFragment

--- app/src/main/java/com/arcmaksim/weatherapp/AlertDialogFragment.kt
+++ app/src/main/java/com/arcmaksim/weatherapp/AlertDialogFragment.kt
@@ -1,13 +1,20 @@
 package com.arcmaksim.weatherapp
 
+import android.app.AlertDialog
 import android.app.Dialog
 import android.app.DialogFragment
+import android.content.Context
 import android.os.Bundle
 
 class AlertDialogFragment : DialogFragment() {
 
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
-        return super.onCreateDialog(savedInstanceState)
+        val context: Context = activity
+        val alertDialogBuilder: AlertDialog.Builder = AlertDialog.Builder(context)
+                .setTitle(R.string.error_title)
+                .setMessage(R.string.error_message)
+                .setPositiveButton(R.string.error_ok_button_text, null)
+        return alertDialogBuilder.create()
     }
 
 }
