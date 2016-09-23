f4806ba84e00b44f9387fe0fbe926ae8d604da19
Maxim Artsybasov <arcmaksim@gmail.com>
2016-09-23 22:49:00 +0300

    Added AlertDialogFragment

---
+++ app/src/main/java/com/arcmaksim/weatherapp/AlertDialogFragment.kt
@@ -0,0 +1,13 @@
+package com.arcmaksim.weatherapp
+
+import android.app.Dialog
+import android.app.DialogFragment
+import android.os.Bundle
+
+class AlertDialogFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState)
+    }
+
+}
