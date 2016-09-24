b0bc422aaa31ec8077ef8685b5030de63c1e93de
Maxim Artsybasov <arcmaksim@gmail.com>
2016-09-24 15:40:54 +0300

    Added check for internet state

--- app/src/main/java/com/arcmaksim/weatherapp/MainActivity.kt
+++ app/src/main/java/com/arcmaksim/weatherapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.arcmaksim.weatherapp
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.widget.Toast
 import okhttp3.*
 import java.io.IOException
 
@@ -28,28 +32,45 @@ class MainActivity : AppCompatActivity() {
 
         val url: String = stringBuilder.toString()
 
-        val okHttpClient: OkHttpClient = OkHttpClient()
-        val request: Request = Request.Builder()
-                .url(url)
-                .build()
+        if (isNetworkAvailable()) {
 
-        val call: Call = okHttpClient.newCall(request)
-        call.enqueue(object: Callback {
-            override fun onFailure(call: Call?, e: IOException?) {}
+            val okHttpClient: OkHttpClient = OkHttpClient()
+            val request: Request = Request.Builder()
+                    .url(url)
+                    .build()
 
-            override fun onResponse(call: Call?, response: Response?) {
-                try {
-                    Log.v(TAG, response?.body()?.string())
-                    if (!response?.isSuccessful!!) {
-                        alertUserAboutError()
-                    }
-                } catch (e: IOException) {
-                    e.printStackTrace()
+            val call: Call = okHttpClient.newCall(request)
+            call.enqueue(object : Callback {
+                override fun onFailure(call: Call?, e: IOException?) {
                 }
-            }
 
-        })
+                override fun onResponse(call: Call?, response: Response?) {
+                    try {
+                        Log.v(TAG, response?.body()?.string())
+                        if (!response?.isSuccessful!!) {
+                            alertUserAboutError()
+                        }
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                }
 
+            })
+
+        } else {
+            Toast.makeText(this, R.string.network_unavailable_message, Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun isNetworkAvailable(): Boolean {
+        val manager: ConnectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo: NetworkInfo? = manager.activeNetworkInfo
+        var isAvailable: Boolean = false
+        if (networkInfo != null) {
+            isAvailable = true
+        }
+        return isAvailable
     }
 
     private fun alertUserAboutError() {
