855f7dd218fe3dedc453aa9010045023add82f1c
Maxim Artsybasov <arcmaksim@gmail.com>
2016-09-24 15:48:05 +0300

    Added CurrentWeather class

---
+++ app/src/main/java/com/arcmaksim/weatherapp/CurrentWeather.kt
@@ -0,0 +1,6 @@
+package com.arcmaksim.weatherapp
+
+class CurrentWeather(val mIcon: String, val mTime: Long, val mTemp: Double,
+                     val mHumidity: Double, val mPrecipChance: Double, val mSummary: String) {
+
+}
