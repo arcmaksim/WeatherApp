223e93e4bd6f8cd4ca598c3c8beb3c46a0f2823b
Maxim Artsybasov <arcmaksim@gmail.com>
2016-09-29 00:11:24 +0300

    Added minor feature

--- app/src/main/java/com/arcmaksim/weatherapp/ui/activities/DailyForecastActivity.kt
+++ app/src/main/java/com/arcmaksim/weatherapp/ui/activities/DailyForecastActivity.kt
@@ -3,6 +3,9 @@ package com.arcmaksim.weatherapp.ui.activities
 import android.app.ListActivity
 import android.os.Bundle
 import android.os.Parcelable
+import android.view.View
+import android.widget.ListView
+import android.widget.Toast
 import com.arcmaksim.weatherapp.R
 import com.arcmaksim.weatherapp.adapters.DayAdapter
 import com.arcmaksim.weatherapp.models.Day
@@ -20,6 +23,16 @@ class DailyForecastActivity : ListActivity() {
         mDays = Arrays.copyOf(parcelables, parcelables.size, Array<Day>::class.java)
         val adapter: DayAdapter = DayAdapter(this, mDays)
         listAdapter = adapter
+    }
+
+    override fun onListItemClick(l: ListView?, v: View?, position: Int, id: Long) {
+        super.onListItemClick(l, v, position, id)
+
+        val message = String.format("On %s the high will be %s and it will be %s",
+                mDays[position].getDayOfTheWeek(),
+                mDays[position].getTemperature(),
+                mDays[position].mSummary)
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 
 }
