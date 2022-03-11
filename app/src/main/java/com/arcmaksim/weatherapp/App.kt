e7c123bd57451659e0d9643fe28ca1d9474f176b
arcmaksim <arcmaksim@gmail.com>
2022-03-11 14:26:08 +0100

    Updated import of DaggerForecastComponent

--- app/src/main/java/com/arcmaksim/weatherapp/App.kt
+++ app/src/main/java/com/arcmaksim/weatherapp/App.kt
@@ -1,7 +1,7 @@
 package com.arcmaksim.weatherapp
 
 import android.app.Application
-import com.arcmaksim.weatherapp.di.DaggerForecastComponent
+import com.arcmaksim.weatherapp.di.component.DaggerForecastComponent
 import com.arcmaksim.weatherapp.di.component.ForecastComponent
 
 class App : Application() {
